850c141d85e5c894d15d13cae00ac26f2e11fabf
nickonator <135019927+nickonator@users.noreply.github.com>
2025-02-12 08:21:01 +0000

    fixed bugs and password hashing

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, make_response
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from flask_cors import CORS
 import sqlite3
 from email.message import EmailMessage
 import smtplib
@@ -9,15 +8,14 @@ import ssl
 import random
 import bcrypt
 
+
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'super-secret-key'
 jwt = JWTManager(app)
-CORS(app, supports_credentials=True)
 
 def UserNameCheck(user):
     connection = sqlite3.connect('/workspaces/HAT1/database/database.db')
     cursor = connection.cursor()
-    #cursor.execute("INSERT INTO users VALUES ('nikhil', 'nikhilj54545@gmail.com', 'secretpassword')")
     cursor.execute("SELECT * FROM users WHERE username= ?", (user,))
     CheckUser = cursor.fetchone()
     connection.close()
@@ -36,6 +34,24 @@ def PasswordCheck(password):
         return True
     return False
 
+def PasswordHash(password):
+    salt = bcrypt.gensalt()
+    password = bcrypt.hashpw(password.encode(), salt)
+    hashed_password = password.decode()
+    return hashed_password
+
+def PasswordCompare(hashed_password, provided_password):
+    if isinstance(hashed_password, str):
+        hashed_password = hashed_password.encode()
+    return bcrypt.checkpw(provided_password.encode(), hashed_password)
+
+def AddUser(username, email, password):
+    connection = sqlite3.connect('/workspaces/HAT1/database/database.db')
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users VALUES (?, ?, ?)", (username, email, password,)) #FIX THIS
+    connection.commit()
+    connection.close()
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -49,19 +65,21 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
-        password = request.form['password']
+        password = PasswordHash(request.form['password'])
         checkPassword = request.form['re-enter password']
-        if UserNameCheck(username) == None and len(username) >= 5 and EmailCheck(email) == None and password==checkPassword and PasswordCheck(password) == True:
+        if UserNameCheck(username) == None and len(username) >= 5 and EmailCheck(email) == None and PasswordCompare(password, checkPassword) == True and PasswordCheck(checkPassword) == True:
+            checkPassword = None
+            AddUser(username, email, password)
             access_token = create_access_token(identity=username)
             response = make_response(render_template("index.html"))
-            response.set_cookie('access_token', access_token, httponly=True, secure=True, samesite='None') 
+            response.set_cookie('access_token_cookie', access_token, httponly=True, secure=True, samesite='None') 
             return response
         else:
             return "unsuccessful"  
     return render_template('signup.html')
 
 @app.route('/bookCatalogue', methods=['GET', 'POST'])
-@jwt_required(locations=["cookies"])
+@jwt_required(locations=['cookies'])
 def bookCatalogue():
     current_user = get_jwt_identity()  
     return render_template('catalogue.html')
